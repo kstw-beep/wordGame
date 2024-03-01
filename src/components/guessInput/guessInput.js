3b70ba8af6adbbc0f6cf27a24b65b81fd7a5df74
Kimberly Schwartz <kimberlyschwartz@kimberlys-mbp.myfiosgateway.com>
2024-03-01 16:35:24 -0500

    exercise 2

--- src/components/guessInput/guessInput.js
+++ src/components/guessInput/guessInput.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function GuessInput() {
-  const [guess, setGuess] = React.useState("");
+function GuessInput({ handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState("");
   function handleSubmit(event) {
     event.preventDefault();
-    console.log({ guess });
-    setGuess("");
+    handleSubmitGuess(tentativeGuess);
+    setTentativeGuess("");
   }
 
   return (
@@ -17,10 +17,10 @@ function GuessInput() {
         maxLength={5}
         pattern="[a-zA-Z]{5}"
         title="5 letter word"
-        value={guess}
+        value={tentativeGuess}
         onChange={(event) => {
           const nextGuess = event.target.value.toUpperCase();
-          setGuess(nextGuess);
+          setTentativeGuess(nextGuess);
         }}
         id="guess-input"
         type="text"

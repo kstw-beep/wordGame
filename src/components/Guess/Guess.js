9b46195dacd9acc3df67f36da9994dd54622406d
Kimberly Schwartz <kimberlyschwartz@kimberlys-mbp.myfiosgateway.com>
2024-03-05 17:46:25 -0500

    exercise 5 all working

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { range } from "../../utils";
-import { checkGuess } from "../../game-helpers";
+import React from 'react';
+
+import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
 function Cell({ letter, status }) {
-  const className = status ? "cell ${status}" : "cell";
+  const className = status ? `cell ${status}` : 'cell';
 
   return <span className={className}>{letter}</span>;
 }
@@ -11,7 +12,7 @@ function Cell({ letter, status }) {
 function Guess({ value, answer }) {
   const result = checkGuess(value, answer);
   return (
-    <p className="guess">
+    <p className='guess'>
       {range(5).map((num) => (
         <Cell
           key={num}
--- src/components/guessInput/guessInput.js
+++ src/components/guessInput/guessInput.js
@@ -1,29 +1,30 @@
-import React from "react";
+import React from 'react';
 
-function GuessInput({ handleSubmitGuess }) {
-  const [tentativeGuess, setTentativeGuess] = React.useState("");
+function GuessInput({ gameStatus, handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState('');
   function handleSubmit(event) {
     event.preventDefault();
     handleSubmitGuess(tentativeGuess);
-    setTentativeGuess("");
+    setTentativeGuess('');
   }
 
   return (
-    <form onSubmit={handleSubmit} className="guess-input-wrapper">
-      <label htmlFor="guess-input">Enter guess:</label>
+    <form onSubmit={handleSubmit} className='guess-input-wrapper'>
+      <label htmlFor='guess-input'>Enter guess:</label>
       <input
         required
+        disabled={gameStatus !== 'running'}
         minLength={5}
         maxLength={5}
-        pattern="[a-zA-Z]{5}"
-        title="5 letter word"
+        pattern='[a-zA-Z]{5}'
+        title='5 letter word'
         value={tentativeGuess}
         onChange={(event) => {
           const nextGuess = event.target.value.toUpperCase();
           setTentativeGuess(nextGuess);
         }}
-        id="guess-input"
-        type="text"
+        id='guess-input'
+        type='text'
       />
     </form>
   );
